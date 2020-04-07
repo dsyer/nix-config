94693b611b9e9dd080091f81516c534599516b00
Dave Syer <dsyer@pivotal.io>
2020-04-07 10:04:54 +0100

    Add jdk11 and re-arrange some bits in vscode

--- nix/code.nix
+++ nix/code.nix
@@ -1,18 +1,22 @@
-# Install VS Code and make it work with a remote that is also running NixOS.
-# You need to launch code this way to make it work on a NixOS remote, or
-# else manually patch the remote by linking node (12) to ~/.vscode-server/bin/*/
+# Install VS Code
 { config, pkgs, ... }:
 
 let
   extensions = (with pkgs.vscode-extensions; [
+      # Make it work with a remote that is also running NixOS.
+      # You need to launch code this way to make it work on a NixOS remote, or
+      # else manually patch the remote by linking node (12) to ~/.vscode-server/bin/*/
       ms-vscode-remote.remote-ssh
-    ]);
+    ])
+    # ~/.nix-defexpr/channels/nixpkgs/pkgs/misc/vscode-extensions/update_installed_exts.sh | tee nix/extensions.nix
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace (import ./extensions.nix).extensions;
   vscode-with-extensions = pkgs.vscode-with-extensions.override {
       vscodeExtensions = extensions;
   };
 in {
 
   environment.systemPackages = with pkgs; [
+    jdk11
     nodejs-12_x
     vscode-with-extensions
   ];
--- nix/remote.nix
+++ nix/remote.nix
@@ -1,11 +1,11 @@
-# Make VS Code work with a remote client
 { config, pkgs, ... }: {
 
-
 environment.systemPackages = with pkgs; [
+  jdk11
   nodejs-12_x
 ];
 
+# Make VS Code work with a remote client
 environment.loginShellInit = ''
   for f in $HOME/.vscode-server/bin/*; do 
     ln -nfs /run/current-system/sw/bin/node $f;

c8249741402e166aa0ddca255dd3c0988b813328
Dave Syer <dsyer@pivotal.io>
2020-04-28 15:33:41 +0100

    Add user to docker group

--- templates/nix/users.nix
+++ templates/nix/users.nix
@@ -1,16 +1,18 @@
 { config, pkgs, ... }: {
 
-  users.groups.dsyer = { name = "${USER}"; members = [ "${USER}" ]; gid = 1000; };
+  users.groups.dsyer = {
+    name = "${USER}";
+    members = [ "${USER}" ];
+    gid = 1000;
+  };
 
   users.extraUsers.dsyer = {
     isNormalUser = true;
     uid = 1000;
     group = "${USER}";
     shell = pkgs.bash;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
-        "${KEY}"
-    ];
+    extraGroups = [ "wheel" "docker" ];
+    openssh.authorizedKeys.keys = [ "${KEY}" ];
   };
 
 }

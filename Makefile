38571cc0dc3821e514530e72be15d3848271b928
Dave Syer <dsyer@pivotal.io>
2020-04-28 16:43:05 +0100

    Fix Makefile

--- Makefile
+++ Makefile
@@ -4,18 +4,18 @@ key = `cat ~/.ssh/id_rsa.pub`
 email = dsyer@pivotal.io
 name = "Dave Syer"
 
-dotfiles = $(shell (cd templates; find dotfiles -type f))
+home = $(shell (cd templates; find home -type f))
 
-all: nix/users.nix $(dotfiles)
+all: nix/users.nix $(home)
 
 nix/users.nix: ~/.ssh/id_rsa.pub
 	cat templates/nix/users.nix | KEY=$(key) envsubst > nix/users.nix
 
-dotfiles/%: templates/dotfiles/%
+home/%: templates/home/%
 	cat $< | EMAIL=$(email) NAME=$(name) envsubst > $@
 	LANG=C stow -v 2 -t ~ -S home
 
 clean:
 	rm -f nix/users.nix
-	for f in $(shell (cd templates/dotfiles; find . -type f)); do rm -f ~/$$f; done
-	for f in $(shell (cd templates/dotfiles; find . -type f)); do rm -f dotfiles/$$f; done
+	for f in $(shell (cd templates/home; find . -type f)); do rm -f ~/$$f; done
+	for f in $(shell (cd templates/home; find . -type f)); do rm -f home/$$f; done
--- nix/users.nix
+++ nix/users.nix
@@ -12,9 +12,7 @@
     group = "dsyer";
     shell = pkgs.bash;
     extraGroups = [ "wheel" "docker" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4yvDJ+mZfpMbCaPuZJ4jVmFHbTiN3ksCOvWj/4n9occuM0hMSqGlIvQ3686XB2ZUmxnN7Z4LGNS4eFYTdRZ6XXoEfdXXJBKMKLZwr5YBJasIV7bBiTFjX6lJDOkzRK0G5qyjO29z2nW3JfoReBXLzOOITuLj0bjWZTUsdrQ4tmOlPUtVe30yql/06YWcdn0jII1PASDw2yrRvbeOFM/nig3zElzb6+m8V5Y9BQ5HyDd6sdMTCwWiYWC1/S6EOmvB3HadbeNdH4LjoEgXGBJ/6u5icavpWQOmFQ5M/ZLkkfokkCIQQEIUHdFVGx5y4myvVPWEGQ79aOpVrBr1WxaJDw== dsyer_rsa"
-    ];
+    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4yvDJ+mZfpMbCaPuZJ4jVmFHbTiN3ksCOvWj/4n9occuM0hMSqGlIvQ3686XB2ZUmxnN7Z4LGNS4eFYTdRZ6XXoEfdXXJBKMKLZwr5YBJasIV7bBiTFjX6lJDOkzRK0G5qyjO29z2nW3JfoReBXLzOOITuLj0bjWZTUsdrQ4tmOlPUtVe30yql/06YWcdn0jII1PASDw2yrRvbeOFM/nig3zElzb6+m8V5Y9BQ5HyDd6sdMTCwWiYWC1/S6EOmvB3HadbeNdH4LjoEgXGBJ/6u5icavpWQOmFQ5M/ZLkkfokkCIQQEIUHdFVGx5y4myvVPWEGQ79aOpVrBr1WxaJDw== dsyer_rsa" ];
   };
 
 }

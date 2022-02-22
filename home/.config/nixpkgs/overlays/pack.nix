4e9ed6e11d7d9c3a459c38fa8f6ec3b99b0f9c26
Dave Syer <dsyer@vmware.com>
2022-02-22 12:38:02 +0000

    Accumulated changes

--- home/.config/nixpkgs/overlays/pack.nix
+++ home/.config/nixpkgs/overlays/pack.nix
@@ -2,12 +2,12 @@ self: super: {
 
   pack = super.stdenv.mkDerivation {
     pname = "pack";
-    version = "0.14.2";
+    version = "0.23.0";
     src = super.fetchurl {
       # nix-prefetch-url this URL to find the hash value
       url =
-        "https://github.com/buildpacks/pack/releases/download/v0.14.2/pack-v0.14.2-linux.tgz";
-      sha256 = "06xbqs34r26qvmkqwmxfgxsbxvckg4ryw05hbdkjyp41dw86q5yb";
+        "https://github.com/buildpacks/pack/releases/download/v0.23.0/pack-v0.23.0-linux.tgz";
+      sha256 = "1vkm0fbk66k8bi5pf4hkmq7929y5av3lh0xj3wpapj2fry18j9yi";
     };
     phases = [ "installPhase" "patchPhase" ];
     installPhase = ''

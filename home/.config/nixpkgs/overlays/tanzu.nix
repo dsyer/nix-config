023d67ac9281b7d7340d5359c6441f4478b26468
Dave Syer <dsyer@vmware.com>
2023-05-23 09:55:28 +0100

    Tweak

--- home/.config/nixpkgs/overlays/tanzu.nix
+++ home/.config/nixpkgs/overlays/tanzu.nix
@@ -1,19 +1,21 @@
 self: super: {
 
-  tanzu-cli = super.stdenv.mkDerivation {
-    pname = "tanzu-cli";
+  tanzu-cli = let
     version = "0.90.0-alpha.2";
+  in super.stdenv.mkDerivation {
+    pname = "tanzu-cli";
+    version = "${version}";
     src = super.fetchurl {
       # nix-prefetch-url this URL to find the hash value
       url =
-        "https://github.com/vmware-tanzu/tanzu-cli/releases/download/v0.90.0-alpha.2/tanzu-cli-linux-amd64.tar.gz";
+        "https://github.com/vmware-tanzu/tanzu-cli/releases/download/v${version}/tanzu-cli-linux-amd64.tar.gz";
       sha256 = "sha256-K588aFd58N/zRNR1ThVN8GHcbkSo6V3xfMg48O7tLYg=";
     };
     phases = [ "installPhase" ];
     installPhase = ''
       mkdir -p $out/bin
       tar -zxf $src
-      cp v0.90.0-alpha.2/tanzu-cli* $out/bin/tanzu
+      cp v${version}/tanzu-cli* $out/bin/tanzu
     '';
   };
 
